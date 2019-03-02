4390dd3fdd180c9eebba826d55405c652ade5681
Xavier Llanga <xavierllanga@hotmail.com>
2019-03-02 20:16:19 +0100

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  nombre: string = 'Xavier';
+  apellido: string = 'Llanga';
+
 }
